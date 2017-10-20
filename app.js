8686f44f371c93c8465beafcb0f52584e15c0545
Amlan <amlan4uonly@gmail.com>
2017-10-20 20:55:42 +0530

    content canges

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('Hi Amlan!, You are Awesome.... :)');
+  response.send('Hi Amlan!, You are Awesome.... I want more from You.)');
 });
 
 app.listen(app.get('port'), function() {
